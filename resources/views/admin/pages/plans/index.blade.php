6bee6af761983766563b29cbcfcd6f4adcfa3d95
rodrigo730 <ro783073@gmail.com>
2022-03-14 22:37:21 -0300

    Atualização

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashbord')
+
+@section('content_header')
+<h1>Dashbord</h1>
+@stop
+
+@section('content')
+<p>Welcome to this beautiful admin papel.</p>
+@stop 
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PlanController;
 
-//Route::get('admin/plans', 'Admin\PlanController@index')->name('plans') ; 
+Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
 
 Route::get('/', function () {
     return view('welcome');
